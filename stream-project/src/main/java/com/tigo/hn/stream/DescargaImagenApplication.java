b8333746ad57e63b0ef8e441daf4bf225efb2ec0
Alex Espinal <alex.espinal.hn@gmail.com>
2025-06-10 16:34:37 -0600

    Primer commit del microservicio Spring Boot

---
+++ stream-project/src/main/java/com/tigo/hn/stream/DescargaImagenApplication.java
@@ -0,0 +1,11 @@
+package com.tigo.hn.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescargaImagenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DescargaImagenApplication.class, args);
+    }
+}
---
+++ stream-project/src/main/java/com/tigo/hn/stream/ServletInitializer.java
@@ -0,0 +1,12 @@
+// Project: stream-project
+package com.tigo.hn.stream;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DescargaImagenApplication.class);
+    }
+} 
